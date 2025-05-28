7eafc12a2546b6b47a744622e79015bcc261daed
Karun-DM <karundm123@gmail.com>
2025-05-28 16:13:36 +0530

    User Controller added

--- src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/service/User/UserService.java
+++ src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/service/User/UserService.java
@@ -1,16 +1,22 @@
 package com.Bridgelabz.DigitalSupplyChainTracker.service.User;
-
+import com.Bridgelabz.DigitalSupplyChainTracker.entity.User;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.Bridgelabz.DigitalSupplyChainTracker.Utility.Role;
 import com.Bridgelabz.DigitalSupplyChainTracker.dto.LoginDto.LoginRequestDto;
 import com.Bridgelabz.DigitalSupplyChainTracker.dto.UserDto.UserRequestDto;
+import com.Bridgelabz.DigitalSupplyChainTracker.entity.User;
 
-
+import java.util.*;
 
 @Service
 public interface UserService {
 	public ResponseEntity<?> RegisterUser(UserRequestDto user);
 	public ResponseEntity<?> LoginUser(LoginRequestDto user);
+	public ResponseEntity<List<User>> getAllUsers();
+	public ResponseEntity<?> updateUserRole(int id,Role role);
+	public ResponseEntity<?> deleteUser(int id);
+	
 }
