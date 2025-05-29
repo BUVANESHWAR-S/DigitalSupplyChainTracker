895540b5444d0e04b33fcfdc3a5b5668656a51c7
dinablaze2004@gmail.com <dinablaze2004@gamil.com>
2025-05-29 14:20:55 +0530

    new mail

--- src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/service/Email/EmailService.java
+++ src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/service/Email/EmailService.java
@@ -13,7 +13,7 @@ public class EmailService {
 
     public void send(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("jawahar6343@gmail.com"); // Must match spring.mail.username
+        message.setFrom("dsct.alerts@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
