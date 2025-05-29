0b2c3e030738cd758f638c91c706f6fd4e061584
Karun-DM <karundm123@gmail.com>
2025-05-29 17:26:29 +0530

    bug fixes

--- src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/entity/Item.java
+++ src/main/java/com/Bridgelabz/DigitalSupplyChainTracker/entity/Item.java
@@ -81,6 +81,22 @@ public class Item {
 		return "Item [id=" + id + ", name=" + name + ", category=" + category + ", supplier=" + supplier
 				+ ", createdDate=" + createdDate + "]";
 	}
+
+	public Item(int id, String name, String category, User supplier, LocalDateTime createdDate) {
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.supplier = supplier;
+		this.createdDate = createdDate;
+	}
+
+	public Item() {
+		
+	}
+	
+	
+	
+	
     
     
     
